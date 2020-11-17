0267ec03031da252b4f7c34c1f66a4d662c0b80d
Javier Lopez <javierlopez@MacBook-Pro.home>
2020-11-17 18:54:03 +0000

    8.6 Center Method

--- typos.rb
+++ typos.rb
@@ -1,22 +1,25 @@
 #Put all student into an array
+#8.5 Add more information: hobbies, country of birth, height, etc.
 students = [
-  { name: "Dr. Hannibal Lecter", cohort: :november },
-  { name: "Darth Vader", cohort: :november },
-  { name: "Nurse Ratched", cohort: :november },
-  { name: "Michael Corleone", cohort: :november },
-  { name: "Alex DeLarge", cohort: :november },
-  { name: "The Wicked Witch of the West", cohort: :november },
-  { name: "Terminator", cohort: :november },
-  { name: "Freddy Krueger", cohort: :november },
-  { name: "The Joker", cohort: :november },
-  { name: "Joffrey Baratheon", cohort: :november },
-  { name: "Norman Bates", cohort: :november },
+  { name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
+  { name: "Darth Vader", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
+  { name: "Nurse Ratched", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
+  { name: "Michael Corleone", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
+  { name: "Alex DeLarge", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
+  { name: "The Wicked Witch of the West", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
+  { name: "Terminator", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
+  { name: "Freddy Krueger", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
+  { name: "The Joker", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
+  { name: "Joffrey Baratheon", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
+  { name: "Norman Bates", cohort: :november, hobbies: "Football", country: "Mexico", height: "169cm" },
 ]
 
 def print_header
   #We print the list of students
-  puts "The students of Makers Academy"
-  puts "______________________________"
+  puts "____________________________________________________"
+  puts "The students of Makers Academy".center(50)
+  puts "----------------------------------------------------"
+  puts "Index" + "Name".center(10) + "Cohort".center(10) + "Hobbies".center(10)
 end
 
 def print(students)
@@ -27,10 +30,19 @@ def print(students)
   option = gets.chomp.capitalize
   case option
   when "See"
-    #8.1 - Modify the program to print a number before the name of each student, e.g. "1. Dr. Hannibal Lecter" Hint: look into each_with_index()
+    #8.1 - Modify the program to print a number before the name of each student, e.g. "1. Dr. Hannibal Lecter" Hint: look into each.with_index()
+    print_header
     students.each.with_index(1) do |student, index|
-      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index}.".center(5) + "#{student[:name]}".center(10) + "#{student[:cohort]}".center(10) + "#{student[:hobbies]}".center(10)
     end
+
+    # 8.4 Rewrite the each() method that prints all students using while or until control flow methods (Loops).
+    # st = students.length
+    # while st > 0
+    #   student = students.
+    #   puts "#{student[:name]} "
+    #   st -= 1
+    # end
   when "Search"
     #8.2 - Modify your program to only print the students whose name begins with a specific letter.
     puts "Which letter whould you like to search for?"
@@ -78,6 +90,6 @@ end
 
 #We call the methods
 students = input_students
-print_header
+
 print(students)
 print_footer(students)
