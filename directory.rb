201411b016ed10f225cf04f80997bcf2581a264f
Javier Lopez <javierlopez@MacBook-Pro.home>
2020-11-17 10:43:41 +0000

    Adding ontent to the ruby file

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#We print the list of students
 puts "The students of Makers Academy"
 puts "______________________________"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#We print the total number of students
+print "Overall, we have "
+print 11
+print " great students"
