331709323bfd12e768c49bb8aed9a720132cc29d
Javier Lopez <javierlopez@MacBook-Pro.home>
2020-11-17 10:58:25 +0000

    Addint student array and iteration

--- directory.rb
+++ directory.rb
@@ -1,18 +1,22 @@
+#Put all student into an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex Delarge",
+  "The Wicked Witch of the west",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Barat,heon",
+  "Norman Bates",
+]
 #We print the list of students
 puts "The students of Makers Academy"
 puts "______________________________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the west"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
 #We print the total number of students
-print "Overall, we have "
-print 11
-print " great students"
+puts "Overall, we have #{students.count} great student"
